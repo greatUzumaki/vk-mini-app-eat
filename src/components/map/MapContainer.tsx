6d2d2f6c87bf4558bbc4e3174b93bcffd7fd009e
Alexey Pargachev <pargachev20@gmail.com>
2022-06-05 19:38:34 +0700

    add max bounds for map

--- src/components/map/MapContainer.tsx
+++ src/components/map/MapContainer.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { LatLngBoundsExpression, LatLngExpression } from 'leaflet';
 
 /** Координаты центра СПБ */
-const center = { lat: 59.938058, lng: 30.315079 };
+const center: LatLngExpression = { lat: 59.938058, lng: 30.315079 };
+
+/** Ограничения карты */
+const maxBoundsCoords: LatLngBoundsExpression = [
+  [60.303665, 29.327367],
+  [59.574302, 30.999731],
+];
 
 export const Map = () => {
   return (
     <MapContainer
+      minZoom={12}
+      maxBounds={maxBoundsCoords}
       style={{ height: '100vh', width: '100%' }}
       zoom={11}
       center={center}
+      maxBoundsViscosity={1.0}
+      inertiaDeceleration={5000}
+      // worldCopyJump={true}
+      attributionControl={false}
     >
       <TileLayer
         url={`https://tile.jawg.io/jawg-streets/{z}/{x}/{y}{r}.png?access-token=${
