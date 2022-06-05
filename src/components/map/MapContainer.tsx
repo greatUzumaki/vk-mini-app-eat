a2fa98edc3f9ad269862c472f153a9bf2eb76828
Alexey Pargachev <pargachev20@gmail.com>
2022-06-05 21:29:23 +0700

    add marker cluster

--- src/components/map/MapContainer.tsx
+++ src/components/map/MapContainer.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import 'react-leaflet-markercluster/dist/styles.min.css';
 import { LatLngBoundsExpression, LatLngExpression, Icon } from 'leaflet';
 import MarkerIcon from '../../assets/icons/marker-icon.png';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 /** Координаты центра СПБ */
 const center: LatLngExpression = { lat: 59.938058, lng: 30.315079 };
@@ -37,12 +39,29 @@ export const Map = () => {
         }`}
       ></TileLayer>
 
-      <Marker
-        riseOnHover
-        riseOffset={999}
-        position={center}
-        icon={markerIcon}
-      ></Marker>
+      <MarkerClusterGroup
+        disableClusteringAtZoom={15}
+        spiderLegPolylineOptions={{ weight: 1.8, color: '#222', opacity: 0.8 }}
+      >
+        <Marker
+          riseOnHover
+          riseOffset={999}
+          position={center}
+          icon={markerIcon}
+        ></Marker>
+        <Marker
+          riseOnHover
+          riseOffset={999}
+          position={{ lat: 59.938058, lng: 30.315179 }}
+          icon={markerIcon}
+        ></Marker>
+        <Marker
+          riseOnHover
+          riseOffset={999}
+          position={{ lat: 59.938058, lng: 30.315279 }}
+          icon={markerIcon}
+        ></Marker>
+      </MarkerClusterGroup>
     </MapContainer>
   );
 };
