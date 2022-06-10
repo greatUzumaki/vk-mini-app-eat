d42d5891a60c1a711cb6486d0f90c9215bb303da
Alexey Pargachev <pargachev20@gmail.com>
2022-06-11 01:14:43 +0700

    add pull to req

--- src/pages/FoodInfo.tsx
+++ src/pages/FoodInfo.tsx
@@ -25,11 +25,17 @@ import {
   PanelHeader,
   PanelHeaderBack,
   PanelProps,
+  PullToRefresh,
   SimpleCell,
+  Tabs,
+  TabsItem,
+  TabsItemProps,
+  UsersStack,
 } from '@vkontakte/vkui';
 import React, { useState } from 'react';
 import { Result } from '../api';
 import { foodInfo } from '../store';
+import AvatarImg from '../assets/images/avatar.jpg';
 
 interface IFoodInfo {
   info: Result;
@@ -81,61 +87,104 @@ const MainInfo = ({ info }: IFoodInfo) => {
   );
 };
 
-export const FoodInfo: React.FC<PanelProps> = ({ nav }: PanelProps) => {
-  const info = useAtomValue(foodInfo);
+const CustomTabs = ({
+  title,
+  props,
+}: {
+  title: string;
+  props: TabsItemProps;
+}) => {
+  <TabsItem style={{ flex: 1 }} {...props}>
+    {title}
+  </TabsItem>;
+};
 
+const ButtonsGroup = () => {
   const [subscription, setSubscription] = useState(false);
   const [like, setLike] = useState(false);
   const [share, setShare] = useState(3);
+
+  return (
+    <Group separator='show'>
+      <Div style={{ paddingBlock: 0 }}>
+        <ButtonGroup
+          style={{ alignItems: 'center' }}
+          stretched
+          mode='horizontal'
+        >
+          <ButtonGroup style={{ alignItems: 'center' }} mode='horizontal'>
+            <Button
+              onClick={() => setShare((old) => old + 1)}
+              style={{ borderRadius: '15px' }}
+              appearance={'neutral'}
+              size='m'
+              hasHover
+              before={<Icon24ShareOutline />}
+              after={share}
+            />
+            <Button
+              onClick={() => setLike((old) => !old)}
+              style={{ borderRadius: '15px' }}
+              appearance={like ? 'negative' : 'neutral'}
+              size='m'
+              hasHover
+              before={like ? <Icon24Like /> : <Icon24LikeOutline />}
+              after={like ? 19 : 18}
+            />
+          </ButtonGroup>
+          <Button
+            style={{ borderRadius: '15px' }}
+            onClick={() => setSubscription((old) => !old)}
+            size='m'
+            appearance={'accent'}
+            stretched
+            mode={subscription ? 'secondary' : 'primary'}
+            hasHover
+          >
+            {subscription ? 'Отписаться' : 'Подписаться'}
+          </Button>
+        </ButtonGroup>
+      </Div>
+    </Group>
+  );
+};
+
+export const FoodInfo: React.FC<PanelProps> = ({ nav }: PanelProps) => {
+  const info = useAtomValue(foodInfo);
+
+  const [fetching, setFetching] = useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setFetching(true);
+
+    setTimeout(() => {
+      setFetching(false);
+    }, 1000);
+  }, []);
 
   return (
     <Panel nav={nav}>
       <PanelHeader left={<PanelHeaderBack onClick={back} />}>
         {info.name}
       </PanelHeader>
-      <MainInfo info={info} />
 
-      <Group mode='plain' separator='show'>
-        <Div style={{ paddingBlock: 0 }}>
-          <ButtonGroup
-            style={{ alignItems: 'center' }}
-            stretched
-            mode='horizontal'
-          >
-            <ButtonGroup style={{ alignItems: 'center' }} mode='horizontal'>
-              <Button
-                onClick={() => setShare((old) => old + 1)}
-                style={{ borderRadius: '15px' }}
-                appearance={'neutral'}
-                size='m'
-                hasHover
-                before={<Icon24ShareOutline />}
-                after={share}
-              />
-              <Button
-                onClick={() => setLike((old) => !old)}
-                style={{ borderRadius: '15px' }}
-                appearance={like ? 'negative' : 'neutral'}
-                size='m'
-                hasHover
-                before={like ? <Icon24Like /> : <Icon24LikeOutline />}
-                after={like ? 19 : 18}
-              />
-            </ButtonGroup>
-            <Button
-              style={{ borderRadius: '15px' }}
-              onClick={() => setSubscription((old) => !old)}
-              size='m'
-              appearance={'accent'}
-              stretched
-              mode={subscription ? 'secondary' : 'primary'}
-              hasHover
-            >
-              {subscription ? 'Отписаться' : 'Подписаться'}
-            </Button>
-          </ButtonGroup>
-        </Div>
-      </Group>
+      <PullToRefresh onRefresh={onRefresh} isFetching={fetching}>
+        <MainInfo info={info} />
+
+        <ButtonsGroup />
+
+        <Group
+          header={
+            <Tabs>
+              <TabsItem style={{ flex: 1 }} selected>
+                Подписчики
+              </TabsItem>
+              <TabsItem style={{ flex: 1 }}>Новости</TabsItem>
+              <TabsItem style={{ flex: 1 }}>Отзывы</TabsItem>
+            </Tabs>
+          }
+        ></Group>
+      </PullToRefresh>
     </Panel>
   );
 };
